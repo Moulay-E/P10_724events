8f909bd7424ce69bcc9346814a87a8ec7df6325f
Moulay-E <Moulay553E@gmail.com>
2023-09-20 11:24:24 +0200

    add test unitaire

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, within } from "@testing-library/react";
 import Page from "./index";
 
 
@@ -30,70 +30,30 @@ describe("When Form is created", () => {
 });
 
 // adding this test
-describe("When a page is created", () => {
-  
+describe("When a page is created", () => { 
   it("a list of events is displayed",  async() => {
-    // render(<Page />);
-  
-    // //  screen.findByText("#DigitonPARIS");
-    // const { container } = render(<Page />);
-    // expect(container.querySelector("#events")).toBeInTheDocument();
     const { container } = render(<Page />);
-
     const eventsContainer = container.querySelector("#events");
     expect(eventsContainer).toBeInTheDocument();
-  
-
   });
   
   it("a list a people is displayed",   () => {
-    // to implement
     const names =  ["Samira", "Jean-baptiste", "Alice", "Luís", "Christine", "Isabelle"] 
     render(<Page />);
     names.forEach(name => {
       expect(screen.getByText(name)).toBeInTheDocument();
     })
   })
+
   it("a footer is displayed", () => {
-    // to implement
     const { container } = render(<Page />);
     expect(container.querySelector("footer")).toBeInTheDocument();
   })
-  it("an event card, with the last event, is displayed", () => {
-    render(<Page />)
-
-
-    const eventCardImage = screen.getByTestId("card-image-testid");
-    // expect(eventCardImage.src).toBeDefined();
-    // expect(eventCardImage.src).not.toBe("");
-    // to implement
-    // expect(screen.getByText("Notre derniére prestation")).toBeInTheDocument();
+  
+  it("an event card, with the last event, is displayed", async () => {
+    const {container} = render(<Page/>)
+    const footerElement = container.querySelector('footer');
+    const eventCardInFooter = footerElement.querySelector('card-testid');
+    expect(eventCardInFooter).toBeDefined();
   })
 });
-
-// const element = await screen.findByText("Evénement 1");
-// expect(element).toBeInTheDocument();
-//    const event2Element = screen.findByText("Evénement 1");
-// expect(event2Element).toBeInTheDocument();
-//   const event2Element = await screen.findByText(" #DigitonPARIS");
-//   expect(event2Element).toBeInTheDocument();
-// beforeEach(() => {
-  //   jest.resetAllMocks();
-  // });
-  
-  // jest.mock("../../contexts/DataContext", () => {
-    //   const mockData = {
-      //     events: [
-        //       { title: "Evénement 1", date: "2022-05-01T20:28:45.744Z", cover: "image1.jpg" },
-        //       { title: "Evénement 2", date: "2022-06-01T20:28:45.744Z", cover: "image2.jpg" }
-        //     ]
-        //   };
-        //   return { useData: jest.fn(() => ({ data: mockData })) };
-        // const { getByText } = render(<Page />);
-        
-        // Vérifiez si les titres des événements sont affichés
-        //  screen.findByText("Evénement 1");
-        // expect(getByText("Evénement 1")).toBeInTheDocument();
-        // expect(getByText("Evénement 2")).toBeInTheDocument();
-        // expect( await screen.findByText("Evénement 2")).toBeInTheDocument();
-// });
